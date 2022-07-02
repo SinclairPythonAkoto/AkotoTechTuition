e0420eb2870a9adcac65a97c910dd11c295b4132
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-02 20:12:07 +0100

    strings Python file

---
+++ Session-2/strings.py
@@ -0,0 +1,27 @@
+name = "Sinclair"
+
+print(name)
+
+# .lower()
+print(name.lower())
+
+# .upper()
+print(name.upper())
+
+# isupper()
+print(name.isupper())
+
+# islower()
+print(name.islower())
+
+# len()
+print(len(name))
+
+# string index
+print(name[0:3])
+
+# .index()
+print(name.index("c"))
+
+# .replace
+print(name.replace("Sinclair", "John"))
