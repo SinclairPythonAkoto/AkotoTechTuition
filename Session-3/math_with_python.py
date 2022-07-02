4e12b9e518aa15fb3d3701850ae8cf2f9521351d
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-02 23:39:49 +0100

    math with python file

---
+++ Session-3/math_with_python.py
@@ -0,0 +1,62 @@
+# from math import floor, ceil, sqrt
+
+# basic math operators
+print(f"10 multiply by 2: {10 * 2}")
+print(f"10 divide by 2: {10 / 2}")
+print(f"10 add 2: {10 + 2}")
+print(f"10 subtract 2: {10 - 2}")
+
+# exponent / raise to the power of
+print(f"4 to the power of 3: {4 ** 3}")
+
+# modulo
+print(f"10 modulo 3: {10 % 3}")
+
+# order of operations
+print(3 * (4 + 5))
+
+# math built-in function: abs()
+print(abs(-5))
+
+# math built-n function: pow()
+print(pow(4, 3))
+
+# math built-in function: max()
+print(max(10, 199, 50))
+
+# math built-in function: min()
+print(min(8, 90, 31, 5, 948, 198, 16, 258, 10, 809, 3))
+
+# math built-in function: round()
+print(round(1.5))
+
+
+#  math module: floor()
+from math import floor
+
+num1 = floor(3.9)
+print(num1)
+
+
+# math module: ceil()
+from math import ceil
+
+num2 = ceil(4.1)
+print(num2)
+
+
+# math module: sqrt()
+from math import sqrt
+
+num3 = sqrt(16)
+print(num3)
+
+
+# Assignment operators
+num4 = 6
+num4 += 4
+print(num4)
+
+num5 = 5
+num5 *= 10
+print(num5)
